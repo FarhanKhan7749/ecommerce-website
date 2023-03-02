e0133958ce0a70ddbb3ac88e7a66769a886292d0
FarhanKhan7749 <fk828633@gmail.com>
2023-03-02 23:00:23 +0530

    Building Blocks of Ecommerce

--- src/Componet/StoreItem/StoreItem.js
+++ src/Componet/StoreItem/StoreItem.js
@@ -1,9 +1,24 @@
-import { Card } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 
 const StoreItem = (props) =>{
     return(
       <Card>
-        <Card.Img variant="top" src={props.itemImgUrl} height="400px" style={{objectFit: "cover"}}></Card.Img>
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="d-flex justify-content-center align-items-center fs-2" style={{fontFamily:'Times New Roman'}}>{props.itemTag}</Card.Title>
+        </Card.Body>
+        <Card.Img
+          variant="top"
+          className="p-4"
+          src={props.itemImgUrl} 
+          height="300px" 
+          style={{objectFit: "cover"}}>
+        </Card.Img>
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="d-flex justify-content-between align-items-baseline">
+            <spam className="ms-2 text-muted">Price ${props.itemPrice.toFixed(2)}</spam>
+            <Button className="">Add to Cart</Button>
+          </Card.Title>
+        </Card.Body>
       </Card>  
     );
 }
